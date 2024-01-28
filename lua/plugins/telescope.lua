61d513d0f5b2dcd80d4c6e179ab2a9a90bdf162b
lumi <lumi@hujaway>
2024-01-28 14:33:55 +0200

    updating a little, need to check stuff for which-key.nvim

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,15 +7,23 @@ return {
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			require("telescope").setup({
-				--extensions = {
-				--	["ui-select"] = {
-				--		require("telescope.themes"),get_dropdown({}),
-				--	},
-				--},
+				defaults = {
+					mappings = {
+						i = { ["<C-h>"] = "which_key" }
+					}	
+				},
+				pickers = {
+					--find_files = {
+						--theme = "dropdown",
+					--}
+				},
 			})
 			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+			-- vim.keymap.set("n", "<C-p>", builtin.find_files, {})
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 
 			require("telescope").load_extension("ui-select")
 		end,
